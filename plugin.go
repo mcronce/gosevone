680720a18d3916fbd5d884b121434dce8ad71845
Mike Cronce <mcronce@sevone.com>
2017-03-24 17:34:10 -0400

    plugin.go:  Added functionality to create object and indicator types

--- plugin.go
+++ plugin.go
@@ -16,6 +16,7 @@ type ObjectType struct {
 	IsEditable bool `json:"isEditable"`
 	ExtendedInfo json.RawMessage `json:"extendedInfo,omitempty"`
 	IndicatorTypes []IndicatorType `json:"-"`
+	IndicatorTypeMap map[string]uint `json:"-"`
 }
 
 type IndicatorType struct {
@@ -34,7 +35,7 @@ type IndicatorType struct {
 	AllowMaxValue bool `json:"allowMaximumValue"`
 	SyntheticExpression string `json:"syntheticExpression"`
 	SyntheticMaximumExpression string `json:"syntheticMaximumExpression"`
-	ExtendedInfo json.RawMessage `json:"extendedInfo.omitempty"`
+	ExtendedInfo json.RawMessage `json:"extendedInfo,omitempty"`
 }
 
 func (this *SevRest) GetIndicatorTypes(include_extended_info bool, filter map[string]interface{}) ([]IndicatorType, error) /* {{{ */ {
@@ -100,5 +101,85 @@ func (this *SevRest) GetObjectTypes(include_extended_info bool, filter map[strin
 	}
 
 	return array, nil
+} // }}}
+
+func (this *SevRest) CreateIndicatorType(payload *IndicatorType) (uint, error) /* {{{ */ {
+	response, err := this.Rest.Post("plugins/indicatortypes", payload)
+	if(err != nil) {
+		return 0, err
+	}
+
+	var body IndicatorType
+	err = response.Decode(&body)
+	if(err != nil) {
+		return 0, err
+	}
+
+	payload.ID = body.ID
+	return payload.ID, nil
+} // }}}
+
+func (this *SevRest) CreateObjectType(payload *ObjectType) (uint, []uint, error) /* {{{ */ {
+	response, err := this.Rest.Post("plugins/objecttypes", payload)
+	if(err != nil) {
+		return 0, nil, err
+	}
+
+	var body ObjectType
+	err = response.Decode(&body)
+	if(err != nil) {
+		return 0, nil, err
+	}
+
+	payload.ID = body.ID
+	created_indicator_type_ids := make([]uint, 0)
+	for i, _ := range payload.IndicatorTypes {
+		payload.IndicatorTypes[i].ObjectTypeID = payload.ID
+		id, err := this.CreateIndicatorType(&payload.IndicatorTypes[i])
+		if(err != nil) {
+			return 0, nil, err
+		}
+		created_indicator_type_ids = append(created_indicator_type_ids, id)
+	}
+
+	return payload.ID, created_indicator_type_ids, nil
+} // }}}
+
+func (this *ObjectType) NewIndicatorType(name string, is_enabled bool, is_default bool, format string, data_units string, display_units string, description string, allow_max bool) (uint, *IndicatorType) /* {{{ */ {
+	indicator_type := IndicatorType{
+		PluginID : this.PluginID,
+		ObjectTypeID : this.ID,
+		Name : name,
+		IsEnabled : is_enabled,
+		IsDefault : is_default,
+		Format : format,
+		DataUnits : data_units,
+		DisplayUnits : display_units,
+		Description : description,
+		AllowMaxValue : allow_max,
+	}
+	id := uint(len(this.IndicatorTypes))
+	this.IndicatorTypeMap[name] = id
+	this.IndicatorTypes = append(this.IndicatorTypes, indicator_type)
+	return id, &this.IndicatorTypes[id]
+} // }}}
+
+func (this *ObjectType) AddIndicatorType(name string, is_enabled bool, is_default bool, format string, data_units string, display_units string, description string, allow_max bool) /* {{{ */ {
+	id, exists := this.IndicatorTypeMap[name]
+	if(exists) {
+		indicator_type := &this.IndicatorTypes[id]
+		indicator_type.PluginID = this.PluginID
+		indicator_type.ObjectTypeID = this.ID
+		indicator_type.Name = name
+		indicator_type.IsEnabled = is_enabled
+		indicator_type.IsDefault = is_default
+		indicator_type.Format = format
+		indicator_type.DataUnits = data_units
+		indicator_type.DisplayUnits = display_units
+		indicator_type.Description = description
+		indicator_type.AllowMaxValue = allow_max
+	} else {
+		this.NewIndicatorType(name, is_enabled, is_default, format, data_units, display_units, description, allow_max)
+	}
 } // }}}
 
